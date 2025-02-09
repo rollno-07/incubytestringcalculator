162822bf690875370cafffea7fefc46cbe394146
Vivek Rawat <vivekrawat0107@gmail.com>
2025-02-09 16:04:10 +0530

    Added component for StringCalculator UI and added som tailwind css styling

--- src/__test__/stringcalculator.test.js
+++ src/__test__/stringcalculator.test.js
@@ -6,7 +6,7 @@ describe("String Calculator", () => {
   test("displays error for invalid input containing alphabets", () => {
     render(<StringCalculator />);
     const textarea = screen.getByPlaceholderText(
-      "Enter numbers (comma/new line separated)"
+      "Enter numbers"
     );
     fireEvent.change(textarea, { target: { value: "fgh,6,bh,-1,bhvd" } });
     fireEvent.click(screen.getByText("Calculate Sum"));
@@ -15,7 +15,7 @@ describe("String Calculator", () => {
   test("displays error for input containing only alphabets", () => {
     render(<StringCalculator />);
     const textarea = screen.getByPlaceholderText(
-      "Enter numbers (comma/new line separated)"
+      "Enter numbers"
     );
     fireEvent.change(textarea, { target: { value: "bhbadc" } });
     fireEvent.click(screen.getByText("Calculate Sum"));
@@ -25,7 +25,7 @@ describe("String Calculator", () => {
   test("calculates sum when numbers are given across multiple lines", () => {
     render(<StringCalculator />);
     const textarea = screen.getByPlaceholderText(
-      "Enter numbers (comma/new line separated)"
+      "Enter numbers"
     );
     fireEvent.change(textarea, { target: { value: "2\n3\n4" } });
     fireEvent.click(screen.getByText("Calculate Sum"));
@@ -35,7 +35,7 @@ describe("String Calculator", () => {
   test("ignores blank lines but sums valid numbers", () => {
     render(<StringCalculator />);
     const textarea = screen.getByPlaceholderText(
-      "Enter numbers (comma/new line separated)"
+      "Enter numbers"
     );
     fireEvent.change(textarea, { target: { value: "2\n\n5\n" } });
     fireEvent.click(screen.getByText("Calculate Sum"));
@@ -44,9 +44,7 @@ describe("String Calculator", () => {
 
   test("displays error for negative numbers", () => {
     render(<StringCalculator />);
-    const textarea = screen.getByPlaceholderText(
-      "Enter numbers (comma/new line separated)"
-    );
+    const textarea = screen.getByPlaceholderText("Enter numbers");
     fireEvent.change(textarea, { target: { value: "-5,3,-2" } });
     fireEvent.click(screen.getByText("Calculate Sum"));
     expect(
@@ -64,7 +62,7 @@ describe("String Calculator", () => {
   test("should return sum for valid comma-separated numbers", () => {
     render(<StringCalculator />);
     const input = screen.getByPlaceholderText(
-      "Enter numbers (comma/new line separated)"
+      "Enter numbers"
     );
     const button = screen.getByText("Calculate Sum");
 
@@ -76,7 +74,7 @@ describe("String Calculator", () => {
   test("should support custom delimiters", () => {
     render(<StringCalculator />);
     const input = screen.getByPlaceholderText(
-      "Enter numbers (comma/new line separated)"
+      "Enter numbers"
     );
     const button = screen.getByText("Calculate Sum");
 
---
+++ src/components/StringCalculator.jsx
@@ -0,0 +1,104 @@
+"use client";
+import { useState, useRef, useEffect } from "react";
+import Image from "next/image";
+
+export default function StringCalculator() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState(null);
+  const inputRef = useRef(null);
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+  const calculateSum = () => {
+    try {
+      if (!input.trim()) {
+        setError("Enter a valid number.");
+        setResult(null);
+        return;
+      }
+
+      let delimiter = /,|\n/;
+      let numbers = input;
+
+      if (input.startsWith("//")) {
+        const parts = input.split("\n");
+        delimiter = new RegExp(parts[0].slice(2));
+        numbers = parts.slice(1).join("\n");
+      }
+
+      const numArray = numbers
+        .split(delimiter)
+        .map((num) => num.trim())
+        .filter((n) => n !== "");
+      const invalidEntries = numArray.filter((n) => isNaN(n));
+
+      if (invalidEntries.length > 0) {
+        setError(
+          `Invalid input: ${invalidEntries.join(
+            ", "
+          )}. Only numbers are allowed.`
+        );
+        setResult(null);
+        return;
+      }
+
+      const parsedNumbers = numArray.map(Number);
+      const negatives = parsedNumbers.filter((n) => n < 0);
+
+      if (numArray.length === 0) {
+        setError("Please Enter Valid Number");
+        setResult(null);
+        return;
+      }
+
+      if (negatives.length > 0) {
+        setError(`Negative numbers not allowed: ${negatives.join(", ")}`);
+        setResult(null);
+        return;
+      }
+
+      setResult(parsedNumbers.reduce((sum, num) => sum + num, 0));
+      setError(null);
+    } catch (err) {
+      setError("Something went wrong.");
+    }
+  };
+
+  return (
+    <div className="bg-primary p-6 rounded-lg shadow-lg w-full max-w-md">
+      <div className="flex justify-between   ">
+        <h1 className="text-2xl font-semibold text-accent mb-4">
+          String Calculator
+        </h1>
+        <Image
+          src="/assets/Incubyte.png"
+          alt="Icon"
+          width={100}
+          height={200}
+          priority
+          quality={100}
+          className="object-contain pb-8"
+        />
+      </div>
+      <textarea
+        className="w-full p-2  rounded-lg focus:outline-none focus:ring-2 focus:ring-accent  text-white bg-black"
+        rows="4"
+        placeholder="Enter numbers"
+        value={input}
+        ref={inputRef}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button
+        className="w-full mt-4 bg-accent text-primary py-2 rounded-lg hover:bg-accent-hover"
+        onClick={calculateSum}
+      >
+        Calculate Sum
+      </button>
+      {error && <p className="text-red-500 mt-2">{error}</p>}
+      {result !== null && !error && (
+        <p className="text-green-600 mt-2">Result: {result}</p>
+      )}
+    </div>
+  );
+}
